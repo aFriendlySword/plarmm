804454894a1789eaa8c669a85de4b75177929954
aFriendlySword <afriendlysword@gmail.com>
2017-07-12 18:47:53 +0200

    e

--- views/pages/Client.js
+++ views/pages/Client.js
@@ -113,12 +113,9 @@ bbr.onclick = function () {
 
 btnswitch.onclick = function() {
     if (display == "times") {
-        display == "coins";
-        document.getElementById("switch").style.right = "155px";
-        var polyline = document.getElementById("arrow");
-        points = polyline.getAttribute('points');
-        points = '13 9 7 15 13 21';
-        polyline.setAttribute('points', points);
+        display = "coins";              
+    } else {
+        display = "times";
     }
 }
 
